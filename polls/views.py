bb4db6dd5a987d0a22a80c84c6609f4a74809946
gislenelima <vanessa@fiberlink.net.br>
2018-05-11 11:03:05 -0300

    funcionando

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
@@ -27,15 +27,15 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-     question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
+                'question': question,
+                'error_message': "You didn't select a choice.",
+    })
     else:
         selected_choice.votes += 1
         selected_choice.save()
@@ -45,7 +45,7 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))#depois da votação redireciona para a page de resultados
 
 
-#parei em: use o sistema de template
-#https://docs.djangoproject.com/pt-br/2.0/intro/tutorial03/
+        #parei em: use o sistema de template
+        #https://docs.djangoproject.com/pt-br/2.0/intro/tutorial03/
 
 
